3671ac6dd2d407cd7e0eacde12d9cca784e8d8d1
DanielMaku <G00467390@atu.ie>
2025-10-22 13:39:38 +0100

    Updated  Student.java

--- ie/atu/wwek4/Student.java
+++ ie/atu/wwek4/Student.java
@@ -1,5 +1,32 @@
-
 package ie.atu.wwek4;
 
 public class Student {
+    String name, email, studentID ;
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setStudentID(String studentID){
+        this.studentID = studentID;
+    }
+    public String getStudentID(){
+        return studentID;
+    }
+
+    @Override
+    public String toString() {
+        return "Student(" +
+                "name ='" + name + '\'' +
+                ", email ='" + email + '\'' +
+                ", studentID ='" + studentID + '\'' +
+                ')';
+    }
 }
